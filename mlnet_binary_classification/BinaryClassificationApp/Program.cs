9ba1199d0f3916edd2a933583bd97e18f7872481
javiersevball <javier.sevball@gmail.com>
2023-03-31 21:12:14 +0200

    Parse command line arguments.

--- mlnet_binary_classification/BinaryClassificationApp/Program.cs
+++ mlnet_binary_classification/BinaryClassificationApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.CommandLine;
 using BinaryClassificationApp.Model;
 
 namespace BinaryClassificationApp
@@ -12,27 +13,64 @@ namespace BinaryClassificationApp
         #region Fields
 
         /// <summary>
+        /// Field that determines the default input file used to train the model.
+        /// </summary>
+        private static readonly string _trainingFileInputPath = "InputData/yelp_labelled.txt";
+
+        /// <summary>
         /// Field that contains the model instance.
         /// </summary>
         private static ReviewSentimentModel _model = new ReviewSentimentModel("Model.mlnet");
 
         #endregion
 
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("--- ML-NET BINARY CLASSIFICATION EXAMPLE");
+            // Command line parser
+            var root = new RootCommand("ML-NET BINARY CLASSIFICATION EXAMPLE");
+            var trainOption = new Option<bool>("--train", "Train the model.");
+            
+            root.AddOption(trainOption);
+            root.SetHandler((train) =>
+                {
+                    if (train)
+                    {
+                        TrainModel();
+                    }
+                    else
+                    {
+                        ExecutePrediction();
+                    }
+                },
+                trainOption);
 
-            Console.WriteLine("Training...");
-            TrainModel("InputData/yelp_labelled.txt");
+            return await root.InvokeAsync(args);
         }
 
         /// <summary>
         /// Method that trains the ML-NET model.
         /// </summary>
-        /// <param name="inputPath">The input data path.</param>
-        private static void TrainModel(string inputPath)
+        private static void TrainModel()
         {
-            _model.Train(inputPath, '\t', false);
+            Console.WriteLine("Training model...");
+            _model.Train(_trainingFileInputPath, '\t', false);
+        }
+
+        /// <summary>
+        /// Method that executes a prediction asking the user the input comment.
+        /// </summary>
+        private static void ExecutePrediction()
+        {
+            Console.Write("Input comment: ");
+            var comment = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(comment))
+            {
+                throw new Exception("Invalid comment.");
+            }
+
+            // TODO
+            throw new NotImplementedException();
         }
     }
 }
